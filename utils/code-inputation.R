371b487105cdb679028f97fbb8bff214eb6afed2
thalisreboucas <thalisreboucasdeoliveira@gmail.com>
2022-01-22 16:20:49 -0300

    atualização :
    otmização
    
    e melhorias

--- utils/code-inputation.R
+++ utils/code-inputation.R
@@ -6,8 +6,21 @@
 
 
 library(Amelia)
+library(missForest)
 
 # Library used for missing data.
 
 # this library have two way if boostrap to inpunt date and one way is EM.
 
+data.iris <- data("iris")
+
+iris.na <- prodNA(iris, noNA = 0.2) 
+
+amelia_fit <- amelia(iris.na, m=5, parallel = "multicore", noms = "Species")
+
+summary(amelia_fit)
+
+
+hist(iris$Sepal.Width)
+hist(iris.na$Sepal.Width)
+hist(amelia_fit$imputations[[5]]$Sepal.Width)
