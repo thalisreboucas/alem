f72b8feaebf253ad849d172046593dc8b921baf4
thalisreboucas <thalisreboucasdeoliveira@gmail.com>
2022-01-26 08:29:21 -0300

    att

--- server.R
+++ server.R
@@ -6,7 +6,7 @@ server <- function(input, output) {
   
   # Number of trips text in UI
   output$num_trips <- renderText({
-    df.geyser  %>%  summarise(media = mean(eruptions)) 
+    "df.geyser  %>%  summarise(media = mean(eruptions))" 
   })
   
 }
